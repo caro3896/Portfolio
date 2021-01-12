0c9d62382e10ade61ee8bbe50b455ee3485e54c0
Caroline Juhl <caroliner.juhl@MBPtilhCaroline.lan>
2021-01-12 15:34:02 +0100

    js oprettet

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+    document.querySelector("#h2").classList.add("right");
+    document.querySelector("#h1").classList.add("left");
+}
+
+function toggleMenu() {
+    console.log("toggleMenu");
+
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        document.querySelector("#menuknap").textContent = "✕";
+    }
+}
